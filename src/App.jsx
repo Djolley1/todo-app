0b4d52fc414cd8b0cc481ff260fa3b98fc11895d
Demarquies Jolley <ahmir1128@gmail.com>
2024-06-10 18:06:10 -0500

    lab 31 start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Todo from './Components/Todo';
+import { SettingsProvider } from './Context/SettingsContext';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <SettingsProvider>
+        <Todo />
+      </SettingsProvider>
+    );
+  }
+}
